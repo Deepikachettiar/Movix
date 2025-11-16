cb61b9150c8d771a46640aad39fe802fe1bcd592
DEEPIKA K <deepika28226@gmail.com>
2025-11-16 20:29:14 +0530

    Update index.js

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,7 +1,11 @@
 import { Inngest } from "inngest";
 import User from "../models/user.js";
 
-export const inngest = new Inngest({ id: "movie-ticket-booking" });
+export const inngest = new Inngest({
+  id: "movie-ticket-booking",
+  eventKey: process.env.INNGEST_EVENT_KEY,      // ✅ ADD THIS
+  signingKey: process.env.INNGEST_SIGNING_KEY,  // ✅ ADD THIS
+});
 
 // Save user data
 const syncUserCreation = inngest.createFunction(
@@ -10,13 +14,12 @@ const syncUserCreation = inngest.createFunction(
   async ({ event }) => {
     try {
       const { id, first_name, last_name, email_addresses, image_url } = event.data;
-      const userData = {
+      await User.create({
         _id: id,
         email: email_addresses[0].email_address,
         name: `${first_name} ${last_name}`,
         image: image_url,
-      };
-      await User.create(userData);
+      });
     } catch (error) {
       console.error("Error creating user:", error);
       throw error;
@@ -30,8 +33,7 @@ const syncUserDeletion = inngest.createFunction(
   { event: "clerk/user.deleted" },
   async ({ event }) => {
     try {
-      const { id } = event.data;
-      await User.findByIdAndDelete(id);
+      await User.findByIdAndDelete(event.data.id);
     } catch (error) {
       console.error("Error deleting user:", error);
       throw error;
@@ -46,12 +48,12 @@ const syncUserUpdation = inngest.createFunction(
   async ({ event }) => {
     try {
       const { id, first_name, last_name, email_addresses, image_url } = event.data;
-      const userData = {
+
+      await User.findByIdAndUpdate(id, {
         email: email_addresses[0].email_address,
         name: `${first_name} ${last_name}`,
         image: image_url,
-      };
-      await User.findByIdAndUpdate(id, userData);
+      });
     } catch (error) {
       console.error("Error updating user:", error);
       throw error;
@@ -59,5 +61,8 @@ const syncUserUpdation = inngest.createFunction(
   }
 );
 
-// Export all functions
-export const functions = [syncUserCreation, syncUserDeletion, syncUserUpdation];
+export const functions = [
+  syncUserCreation,
+  syncUserDeletion,
+  syncUserUpdation,
+];
