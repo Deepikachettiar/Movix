55c7846697df33bc61c36d0468905882296e4842
Deepikachettiar <deepika28226@gmail.com>
2025-12-22 15:23:39 +0530

    add show movie details to the database using APIs

--- server/controllers/showcontroller.js
+++ server/controllers/showcontroller.js
@@ -1,5 +1,8 @@
-import axios from "axios"
+import axios from "axios";
+import Movie from "../models/movie";
 
+
+//API to get now playing movies
 export const getNowPlayingMovies = async (req, res)=>{
     try {
         const { data } = await axios.get('https://api.themoviedb.org/3/movie/now_playing', {
@@ -9,6 +12,47 @@ export const getNowPlayingMovies = async (req, res)=>{
         const movies = data.results;
         res.json({success: true, movies: movies})
     } catch (error) {
+        console.error(error);
+        res.json({success: false, message: error.message})
+    }
+}
+
+//API to add a show to database
+export const addShow = async (req, res)=>{
+    try {
+        const { movieId, showsInput,showPrice } = req.body;   
+        let movie=await Movie.findById(movieId);
+        if(!movie)
+        {
+              const [movieDetailsResponse, movieCreditsResponse] = await Promise.all([
+                axios.get(`https://api.themoviedb.org/3/movie/{movie_id}`, {
+                    headers: { Authorization : `Bearer ${process.env.TMDB_API_KEY}` }
+                }),
+                axios.get(`https://api.themoviedb.org/3/movie/${movieId}/credits`, {
+                    headers: { Authorization : `Bearer ${process.env.TMDB_API_KEY}` }
+                })
+            ]);
+
+            const movieApiData = movieDetailsResponse.data;
+            const movieCreditsData = movieCreditsResponse.data;
+
+            const movieDetails={
+                _id:movieId,
+                title:movieApiData.title,
+                overview:movieApiData.overview,
+                poster_path:movieApiData.poster_path,
+                backdrop_path:movieApiData.backdrop_path,
+                release_date:movieApiData.release_date,
+                original_language:movieApiData.original_language,
+                tagline:movieApiData.tagline||"",
+                genres:movieApiData.genres,
+                cast:movieCreditsData.cast,
+                vote_avg:movieApiData.vote_average,
+                runtime:movieApiData.runtime,
+
+            }
+        }
+    }catch (error) {
         console.error(error);
         res.json({success: false, message: error.message})
     }
