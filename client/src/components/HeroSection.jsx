dcd035eb1a82b8fdf96783e776fbffe9ca4e2dc6
Deepikachettiar <deepika28226@gmail.com>
2026-07-02 16:42:54 +0530

    updated

--- client/src/components/HeroSection.jsx
+++ client/src/components/HeroSection.jsx
@@ -11,27 +11,27 @@ const HeroSection = () => {
   className='flex flex-col items-start justify-center gap-4 
   px-6 md:px-16 lg:px-36 py-20 bg-cover bg-center h-screen'
   style={{
-    backgroundImage: 'url("https://image.tmdb.org/t/p/original/9vY9sD4cQf1l4z313Wnt4H39Sb4y.jpg")',
+    backgroundImage: 'url("https://image.tmdb.org/t/p/original/s4o9bK9qDrKXXWp9dJiDe.jpg")',
   }}
 >
 
-        <span className="text-primary font-bold text-sm tracking-widest mt-20">VIKRAM</span>
+        <span className="text-primary font-bold text-sm tracking-widest mt-20">BIOGRAPHY</span>
     
         <h1 className='text-5xl md:text-[70px] md:leading-18 font-semibold max-w-110'>
-            Vikram
+            Michael
         </h1>
 
         <div className='flex items-center gap-4 text-gray-300'>
-            <span>Action | Thriller</span>
+            <span>Musical | Drama | History</span>
             <div className='flex items-center gap-1'>
-                <CalendarIcon className='w-4.5 h-4.5'/>2022
+                <CalendarIcon className='w-4.5 h-4.5'/>2026
             </div>
             <div className='flex items-center gap-1'>
-                <ClockIcon className='w-4.5 h-4.5'/> 2h 53m
+                <ClockIcon className='w-4.5 h-4.5'/> 2h 7m
             </div>
         </div>
         <p className="max-w-md text-gray-300">
-        A special ops squad is tasked with investigating a series of killings by a masked group of vigilantes, leading to a massive conflict involving drug kings and black ops agents.
+        A comprehensive biographical portrait of Michael Jackson, tracing his journey from child stardom with the Jackson 5 to his status as the legendary King of Pop.
          </p>
 
          <button onClick={()=>navigate('/movies')}
